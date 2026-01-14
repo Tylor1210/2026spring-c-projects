51e74116fa1bbf591786468d5d7d7a0559c89c2f
Tylor C <100950093+Tylor1210@users.noreply.github.com>
2026-01-13 19:56:25 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardIdentifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pbCard1_Click(object sender, EventArgs e)
+        {
+            lbCardName.Text = "King of Spades";
+        }
+
+        private void pbCard2_Click(object sender, EventArgs e)
+        {
+            lbCardName.Text = "Ace of Spades";
+        }
+
+        private void pbCard3_Click(object sender, EventArgs e)
+        {
+            lbCardName.Text = "2 of Clubs";
+        }
+
+        private void pbCard4_Click(object sender, EventArgs e)
+        {
+            lbCardName.Text = "8 of Diamonds";
+        }
+
+        private void pbCard5_Click(object sender, EventArgs e)
+        {
+            lbCardName.Text = "Black Joker";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
